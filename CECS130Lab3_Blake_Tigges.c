9f4785f8ce88d9c3ff273f14f3b22dedb08b4655
Blake <ihackedurtoaster@gmail.com>
2022-01-25 13:42:41 -0500

    fix

--- CECS130Lab3_Blake_Tigges.c
+++ CECS130Lab3_Blake_Tigges.c
@@ -11,9 +11,9 @@ int main() // Main Function
 	int opnum = 0;
 	while (opnum != 7)
 	{
-	float numone = 0; float numtwo = 0; int intone = 0; int inttwo = 0; // Set the float and inte
-	printf("Pick a function: \n (1) Addition \n (2) Subtraction \n (3) Multiplication \n (4) Division \n (5) Modulus (integers only) \n (6) Test if prime (integers only) \n (7) Exit ");
-	printf("Enter operation number: \n");
+	float numone = 0; float numtwo = 0; int intone = 0; int inttwo = 0; // Set the float and integer variables 
+	printf("Pick a function: \n (1) Addition \n (2) Subtraction \n (3) Multiplication \n (4) Division \n (5) Modulus (integers only) \n (6) Test if prime (integers only) \n (7) Exit ");// Display options
+	printf("Enter operation number: \n"); // Ask for what to do
 	scanf("%d", &opnum);
 	switch (opnum){
 		case 1: printf("Enter first number: "); // Addition operation
@@ -48,15 +48,7 @@ int main() // Main Function
 		break;
 		case 6: printf("Enter number to test: "); // Prime number checker
 		scanf("%d", &intone);
-		int primecheck1 = 0; int primecheck2 = 0;
-		if (intone == 1){
-			printf("Not prime 1 x 1 = 1");
-			break;
-		} 
-		else if (intone == 0){
-			printf("Not prime 0 x 1 = 0");
-			break;
-		}
+		int primecheck1 = 2; int primecheck2 = 2;
 		while (intone % primecheck1 != 0 && primecheck1 < 2000000000){
 			primecheck1++;
 			if (primecheck1 == intone){ // make sure it isnt using number as factor for itself 
@@ -69,15 +61,22 @@ int main() // Main Function
 				primecheck2++; 
 			}
 		}
-		if (primecheck1 * primecheck2 != intone){
-			printf("Prime!\n"); // Print prime if it is prime 
+		if (primecheck1 * primecheck2 == intone){
+			printf("Not prime %d x %d = %d \n", primecheck1, primecheck2, intone); // Print factors is not prime
+			break;
+		}
+		else if (intone == 1){
+			printf("Not prime 1 x 1 = 1\n");
+			break;
+		}
+		else if (intone == 0){
+			printf("Not prime 0 x 2 = 0\n");
 			break;
 		}
 		else {
-			printf("Not prime %d x %d = %d \n", primecheck1, primecheck2, intone); // Print factors is not prime
+			printf("Prime!\n"); // Print prime if it is prime 
 		}
-		break;
-
+		break;	
 	}
 	}
 	printf("Good Bye!");
