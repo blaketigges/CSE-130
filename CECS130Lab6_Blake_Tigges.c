bccc08636da13144300a5674e1311dc462411306
Blake <ihackedurtoaster@gmail.com>
2022-02-17 17:50:55 -0500

    a

--- CECS130Lab6_Blake_Tigges.c
+++ CECS130Lab6_Blake_Tigges.c
@@ -7,18 +7,31 @@ Blake Tigges
 #include <stdio.h> 
 #include <stdlib.h>
 
-struct contact
+typedef struct
 	{
 		char firstName[80];
 		char lastName[80];
 		char number[11];
-	} ;
+	} contact;
 
 int main(){
 	int opt = 0;
+	contact arrayOfContacts;
+	contact list; 
+	int newcount = 100;
+	list = realloc(arrayOfContacts , newcount * sizeof(contact) ); 
 	while (opt != 4){
 	printf("Phone Book Application\n    1) Add Friend \n    2) Delete Friend \n    3) Show phone book\n    4) Exit\n");
 	scanf("What do you want to do? %d\n", &opt)
-	
+	switch(opt){
+		case 1:
+			
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		}
 	}
+	free(arrayOfContacts);
 }
