c2110b7d7aec23d12ae03fc090079bd5571ea3e8
Blake <ihackedurtoaster@gmail.com>
2022-02-18 23:58:35 -0500

    cool

--- Untitled2.c
+++ Untitled2.c
@@ -1,12 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-	while(1==1){
-		int *ptr;
-		ptr = calloc(50, sizeof(int));
-		int i;
-		for (i = 1; i < 100; i++){
-		*ptr = 10000;
-		}
-	}
-}
+
