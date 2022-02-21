d5ba5cb029920f24b6537c2d22cef440c6b1d286
Blake <ihackedurtoaster@gmail.com>
2022-02-21 15:38:30 -0500

    dynamic memory stuff but broken

--- CECS130Lab6_Blake_Tigges.c
+++ CECS130Lab6_Blake_Tigges.c
@@ -8,15 +8,17 @@ Blake Tigges
 #include <stdlib.h>
 #include <string.h>
 
-struct phonebook{
+typedef struct{
 		char firstName[80];
 		char lastName[80];
 		char number[11];
-	};
+	} phonebook;
 
 int main(){
-	struct phonebook book[10];
-	
+	phonebook *tBook; // temp pointer 
+	phonebook *dBook = malloc(10 * sizeof(phonebook)); // dynamic pointer
+	int numOfContacts = 10;
+
 	int opt = 0;
 	while (opt != 4){
 	printf("Phone Book Application\n    1) Add Friend \n    2) Delete Friend \n    3) Show phone book\n    4) Exit\n");
@@ -25,12 +27,21 @@ int main(){
 	int c = 0;
 	switch (opt){
 		case 1: // Add first name, last name, and number, then increment c
+			if (c == 10){ 
+				tBook = realloc(dBook, (numOfContacts + 1) * sizeof(phonebook)); // reallocates memory if c = 10
+				if (tBook == NULL){
+					printf("Reallocation failed, out of memory!!\n"); // if realloc fails say it failed
+				} else {
+					dBook = tBook; // if realloc succeeds copy temp book to dynamic book
+					numOfContacts++;
+				}
+			} 
 			printf("Enter First Name: ");
-			scanf("%s", book[c].firstName);
+			scanf("%s", dBook[c].firstName);
 			printf("Enter Last Name: ");
-			scanf("%s", book[c].lastName);
+			scanf("%s", dBook[c].lastName);
 			printf("Enter Phone Number: ");
-			scanf("%s", book[c].number);
+			scanf("%s", dBook[c].number);
 			c++;
 			break;
 		case 2: // ask who to delete, then move everything down one, then decrease c
@@ -42,9 +53,9 @@ int main(){
 			printf("Enter Last Name: ");
 			scanf("%s", delLast);
 			for (int i = 0; i < c; i++){
-				if (strcmp(delFirst, book[i].firstName) == 0 && strcmp(delLast, book[i].lastName) == 0){
+				if (strcmp(delFirst, dBook[i].firstName) == 0 && strcmp(delLast, dBook[i].lastName) == 0){
 					for (int j = i; j < c; j++){
-						book[j] = book[j+1];
+						dBook[j] = dBook[j+1];
 					}
 					printf("Deleted %s %s\n", delFirst, delLast);
 					c--;
@@ -53,15 +64,11 @@ int main(){
 			break;
 		case 3: // print out 0 through c
 			for (int i = 0; i < c; i++){
-				printf("%s %s %s\n", book[i].firstName, book[i].lastName, book[i].number);
+				printf("%s %s %s\n", dBook[i].firstName, dBook[i].lastName, dBook[i].number); 
 			}
 			break;
 		}
 	}
-
-	int newcount = 100;
-	// list = realloc(arrayOfContacts , newcount * sizeof(contact) ); 
-	
-//	free(book);
+	free(dBook);
 	return 0;
 }
