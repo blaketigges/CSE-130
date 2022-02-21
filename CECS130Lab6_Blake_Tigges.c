fccc2db508d14eaa860deb0f9378328263c85685
Blake <ihackedurtoaster@gmail.com>
2022-02-21 16:15:37 -0500

    fixed memory stuff now all working

--- CECS130Lab6_Blake_Tigges.c
+++ CECS130Lab6_Blake_Tigges.c
@@ -15,19 +15,19 @@ typedef struct{
 	} phonebook;
 
 int main(){
+	int numOfContacts = 10; //number of contacts to start with
 	phonebook *tBook; // temp pointer 
-	phonebook *dBook = malloc(10 * sizeof(phonebook)); // dynamic pointer
-	int numOfContacts = 10;
+	phonebook *dBook = malloc(numOfContacts * sizeof(phonebook)); // dynamic pointer
+	int c = 0; // current contact
 
-	int opt = 0;
+	int opt = 0; 
 	while (opt != 4){
 	printf("Phone Book Application\n    1) Add Friend \n    2) Delete Friend \n    3) Show phone book\n    4) Exit\n");
 	printf("Enter option: ");
 	scanf("%d", &opt);
-	int c = 0;
 	switch (opt){
 		case 1: // Add first name, last name, and number, then increment c
-			if (c == 10){ 
+			if (c == numOfContacts){ 
 				tBook = realloc(dBook, (numOfContacts + 1) * sizeof(phonebook)); // reallocates memory if c = 10
 				if (tBook == NULL){
 					printf("Reallocation failed, out of memory!!\n"); // if realloc fails say it failed
@@ -37,11 +37,12 @@ int main(){
 				}
 			} 
 			printf("Enter First Name: ");
-			scanf("%s", dBook[c].firstName);
+			scanf("%s", &dBook[c].firstName);
 			printf("Enter Last Name: ");
-			scanf("%s", dBook[c].lastName);
+			scanf("%s", &dBook[c].lastName);
 			printf("Enter Phone Number: ");
-			scanf("%s", dBook[c].number);
+			scanf("%s", &dBook[c].number);
+			printf("Contact added %s %s #%s\n", dBook[c].firstName, dBook[c].lastName, dBook[c].number);
 			c++;
 			break;
 		case 2: // ask who to delete, then move everything down one, then decrease c
@@ -63,8 +64,10 @@ int main(){
 			}
 			break;
 		case 3: // print out 0 through c
-			for (int i = 0; i < c; i++){
-				printf("%s %s %s\n", dBook[i].firstName, dBook[i].lastName, dBook[i].number); 
+			printf("Phone Book \n");
+			printf("c = %d\n", c);
+			for (int k = 0; k < c; k++){
+				printf("%s %s %s\n", dBook[k].firstName, dBook[k].lastName, dBook[k].number);
 			}
 			break;
 		}
