56162a156e69c63ee7a44da55b9ba11500366d6a
Blake <ihackedurtoaster@gmail.com>
2022-03-01 14:17:25 -0500

    start 7

---
+++ CECS130Lab7_Blake_Tigges.c
@@ -0,0 +1,96 @@
+/*
+Lab 7 - phonebook+    
+3/1/2022
+Section 1A
+Blake Tigges
+*/
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+		char firstName[80];
+		char lastName[80];
+		char number[11];
+	} phonebook;
+
+int main(){
+	int numOfContacts = 10; //number of contacts to start with
+	phonebook *tBook; // temp pointer 
+	phonebook *dBook = malloc(numOfContacts * sizeof(phonebook)); // dynamic pointer
+	int c = 0; // current contact
+
+	int opt = 0; 
+	while (opt != 8){
+	printf("Phone Book Application\n    1) Add Friend \n    2) Delete Friend \n    3) Show phone book\n    4) alphabetize the list\n	5) find phone number for given name\n	6) Randomly pick number to call\n	7) delete everyone\n	8) Exit\n");
+	printf("Enter option: ");
+	scanf("%d", &opt);
+	switch (opt){
+		case 1: // Add first name, last name, and number, then increment c
+			if (c == numOfContacts){ 
+				tBook = realloc(dBook, (numOfContacts + 1) * sizeof(phonebook)); // reallocates memory if c = 10
+				if (tBook == NULL){
+					printf("Reallocation failed, out of memory!!\n"); // if realloc fails say it failed
+				} else {
+					dBook = tBook; // if realloc succeeds copy temp book to dynamic book
+					numOfContacts++;
+				}
+			} 
+			printf("Enter First Name: ");
+			scanf("%s", dBook[c].firstName);
+			printf("Enter Last Name: ");
+			scanf("%s", dBook[c].lastName);
+			printf("Enter Phone Number: ");
+			scanf("%s", dBook[c].number);
+			printf("Contact added %s %s #%s\n", dBook[c].firstName, dBook[c].lastName, dBook[c].number); // confirm contact added
+			c++;
+			break;
+		case 2: // ask who to delete, then move everything down one, then decrease c
+			printf("Who do you want to delete?\n");
+			char delFirst[80];
+			printf("Enter First Name: ");
+			scanf("%s", delFirst);
+			char delLast[80];
+			printf("Enter Last Name: ");
+			scanf("%s", delLast);
+			for (int i = 0; i < c; i++){
+				if (strcmp(delFirst, dBook[i].firstName) == 0 && strcmp(delLast, dBook[i].lastName) == 0){ // find if entered name is in book
+					for (int j = i; j < c; j++){
+						dBook[j] = dBook[j+1]; // if so move everything back one
+					}
+					printf("Deleted %s %s\n", delFirst, delLast); 
+					c--;
+				}
+			}
+			break;
+		case 3: // print out 0 through c
+			printf("Contacts: \n");
+			for (int k = 0; k < c; k++){
+				printf("%s %s %s\n", dBook[k].firstName, dBook[k].lastName, dBook[k].number); // print contacts through c 
+			}
+			break;
+		case 4: // alphabetically sort contacts by name (first or last)
+			printf("Contacts: \n");
+			break;
+		case 5: // find phone number by name
+			printf("Enter First Name: ");
+			char findFirst[80];
+			scanf("%s", findFirst);
+			char findLast[80];
+			printf("Enter Last Name: ");
+			scanf("%s", findLast);
+			for (int i = 0; i < c; i++){
+				if (strcmp(findFirst, dBook[i].firstName) == 0 && strcmp(findLast, dBook[i].lastName) == 0){ // compare entered name with all names until match
+					printf("%s %s #%s\n", dBook[i].firstName, dBook[i].lastName, dBook[i].number); // then print match
+				}
+			}
+			break;
+		case 6: // randomly select a contact and print out their name and number
+			break;
+		case 7: // delete all contacts
+			break;
+		}
+	}
+	free(dBook); // free memory
+	return 0;
+}
