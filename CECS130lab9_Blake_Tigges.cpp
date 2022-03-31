2b9fb66f082de613e3e73e46cc3d477ff5965cca
Blake <ihackedurtoaster@gmail.com>
2022-03-30 23:40:18 -0400

    a

--- CECS130lab9_Blake_Tigges.cpp
+++ CECS130lab9_Blake_Tigges.cpp
@@ -4,4 +4,36 @@ Lab 9 - tic tac toe
 Section 1A
 Blake Tigges
 */
+
+#include <iostream>
 using namespace std;
+class game{  
+    public:
+        void clearBoard(); // function to clear board
+        void printBoard(); // function to print board
+        void checkSpot(char, int); // function to check spot
+    private:
+        char board[3][3]; // 3x3 array for board
+};
+
+int main(){
+    
+}
+
+void clearBoard(){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            board[i][j] = ' ';
+        }
+    }
+}
+void printBoard(){
+    cout << "  A B C" << endl;
+    for(int i = 0; i < 3; i++){
+        cout << i+1 << " ";
+        for(int j = 0; j < 3; j++){
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
