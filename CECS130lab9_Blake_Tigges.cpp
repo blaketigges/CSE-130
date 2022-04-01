c60a5db714795cbfd89dfb16233333b8d3b33f45
Blake <ihackedurtoaster@gmail.com>
2022-04-01 11:03:23 -0400

    fdsjihfjksdda

--- CECS130lab9_Blake_Tigges.cpp
+++ CECS130lab9_Blake_Tigges.cpp
@@ -6,12 +6,16 @@ Blake Tigges
 */
 
 #include <iostream>
+#include <time.h>
 using namespace std;
 class game{  
     public:
         void clearBoard(); // function to clear board
         void printBoard(); // function to print board
         void checkSpot(char, int); // function to check spot
+        void first(); // function to who goes first randomly
+        int turn; // track of turn, 0 = player, 1 = computer
+        int done; // is game still playing
     private:
         char board[3][3]; // 3x3 array for board
 };
@@ -21,6 +25,26 @@ int main(){
     game tboard; // create board object
     tboard.clearBoard(); // clear board
     tboard.printBoard(); // print board
+    while (tboard.done == 0){ // while game is still playing
+        if (tboard.turn == 0){ // if it is player's turn
+            cout << "Player's turn" << endl;
+            char col; // column
+            int row; // row
+            cin >> col; // get spot
+            cin >> row;
+            tboard.checkSpot(col, row); // check spot
+            tboard.printBoard(); // print board
+            tboard.turn = 1; // switch turn
+        }
+        else if (tboard.turn == 1){ // if it is computer's turn
+            cout << "Computer's turn" << endl;
+            srand(time(NULL)); // seed random number generator
+            int spot = rand() % 9; // random spot
+            tboard.checkSpot('O', spot); // check spot
+            tboard.printBoard(); // print board
+            tboard.turn = 0; // switch turn
+        }
+    }
 }
 
 void game::clearBoard(){
@@ -39,4 +63,15 @@ void game::printBoard(){
         }
         cout << endl;
     }
+}
+void game::first(){
+    srand(time(0));
+    turn = rand() % 2;
+    if(turn == 0){
+        cout << "Player goes first" << endl;
+    }
+    else{
+        cout << "Computer goes first" << endl;
+    }
+    
 }
