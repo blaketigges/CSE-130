e5b12cce82aae4fe60214bbd737a960caf6f85d1
Blake <ihackedurtoaster@gmail.com>
2022-04-11 23:07:37 -0400

    start on lab 10, it dont work right

--- CECS130lab10_Blake_Tigges.cpp
+++ CECS130lab10_Blake_Tigges.cpp
@@ -14,7 +14,7 @@ class game{
     public:
         void clearBoard(); // function to clear board
         void printBoard(); // function to print board
-        int checkSpot(int, int); // function to check spot
+        int checkSpot(int, int, int); // function to check spot
         void first(); // function to who goes first randomly
         void flipActive(); // change active letter
         void checkWin(); // has someone won
@@ -23,7 +23,7 @@ class game{
         int done = 0; // is game still playing
         char active = 'X'; // use x or o
     private:
-        char board[3][3]; // 3x3 array for board
+        char board[3][3][3]; // 3x3x3 array for board [col][row][level]
 };
 
 int main(){
@@ -37,11 +37,13 @@ int main(){
             cout << "Player's turn" << endl;
             int col; // column
             int row; // row
+            int lvl; // level
              do { // keep getting new spot until spot is open
                 cout << "Pick column" << endl; cin >> col; // get spot
                 cout << "Pick row" << endl; cin >> row;
-            } while (tboard.checkSpot(col, row) == 0);
-            tboard.checkSpot(col, row); // check spot
+                cout << "Pick level" << endl; cin >> lvl;
+            } while (tboard.checkSpot(col, row, lvl) == 0);
+            tboard.checkSpot(col, row, lvl); // check spot
             tboard.printBoard(); // print board
             tboard.checkWin(); // check for win
             tboard.turn = 1; // switch turn
@@ -52,10 +54,12 @@ int main(){
             srand(time(0)); // seed random number generator
             int col = (rand() % 3) + 1; // random spot
             int row = (rand() % 3) + 1;
+            int lvl = (rand() % 3) + 1;
             do { // keep getting new spot until spot is open
                 col = (rand() % 3) + 1;
                 row = (rand() % 3) + 1;
-            } while (tboard.checkSpot(col, row) == 0);
+                lvl = (rand() % 3) + 1;
+            } while (tboard.checkSpot(col, row, lvl) == 0);
             tboard.printBoard(); // print board
             tboard.checkWin(); // check for win
             tboard.turn = 0; // switch turn
@@ -75,21 +79,23 @@ void game::whoWon(){
 }
 void game::checkWin(){
     // check for win
-    for (int i = 0; i < 3; i++){ // check rows
-        if ((board[i][0] == 'X' || board[i][0] == 'O') && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
-            whoWon();
+    for (int j = 0; j < 3; j++){ // check for wins on each level
+        for (int i = 0; i < 3; i++){ // check rows
+            if ((board[i][0][j] == 'X' || board[i][0][j] == 'O') && board[i][0][j] == board[i][1][j] && board[i][1][j] == board[i][2][j]){
+                whoWon();
+            }
         }
-    }
-    for (int i = 0; i < 3; i++){ // check colums
-        if ((board[0][i] == 'X' || board[0][i] == 'O') && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
-            whoWon();
+        for (int i = 0; i < 3; i++){ // check colums
+            if ((board[0][i][j] == 'X' || board[0][i][j] == 'O') && board[0][i][j] == board[1][i][j] && board[1][i][j] == board[2][i][j]){
+                whoWon();
+            }
         }
-    }
-    if ((board[0][0] == 'X' || board[0][0] == 'O') && board[0][0] == board[1][1] && board[1][1] == board[2][2]){ // check diag 1
-        whoWon(); // check who won
-    }
-    else if ((board[2][0] == 'X' || board[2][0] == 'O') && board[2][0] == board[1][1] && board[1][1] == board[0][2]){ // check column 1
-        whoWon(); // check who won
+        if ((board[0][0][j] == 'X' || board[0][0][j] == 'O') && board[0][0][j] == board[1][1][j] && board[1][1][j] == board[2][2][j]){ // check diag 1
+            whoWon(); // check who won
+        }
+        else if ((board[2][0][j] == 'X' || board[2][0][j] == 'O') && board[2][0][j] == board[1][1][j] && board[1][1][j] == board[0][2][j]){ // check column 1
+            whoWon(); // check who won
+        }
     }
 }
 void game::flipActive(){ // function to change active
@@ -100,31 +106,35 @@ void game::flipActive(){ // function to change active
         active = 'X'; // change to x
     }
 }
-int game::checkSpot(int col, int row){
-    if (board[col-1][row-1] != ' '){
+int game::checkSpot(int col, int row, int lvl){
+    if (board[col-1][row-1][lvl-1] != ' '){
         //cout << "Spot is taken" << endl;
         //cout << col << " " << row << endl;
         return 0;
     } else {
-        board[col -1][row -1] = active;
+        board[col-1][row-1][lvl-1] = active;
         return 1;
     }
 }
 void game::clearBoard(){
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            board[i][j] = ' ';
+            for (int k = 0; k < 3; k++){
+                board[i][j][k] = ' ';
+            }
         }
     }
 }
 void game::printBoard(){
-    cout << endl << "  1 2 3" << endl;
-    for(int i = 0; i < 3; i++){
-        cout << i+1 << " ";
-        for(int j = 0; j < 3; j++){
-            cout << board[j][i] << " ";
+    for (int k = 0; k < 3; k++){
+        cout << endl << "  1 2 3" << endl;
+        for(int i = 0; i < 3; i++){
+            cout << i+1 << " ";
+            for(int j = 0; j < 3; j++){
+                cout << board[j][i][k] << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
 }
 void game::first(){
