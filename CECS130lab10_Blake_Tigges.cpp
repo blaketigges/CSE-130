b18628b3cb98b973b68eaa7d20e0d6c0a1e1fd55
Blake <ihackedurtoaster@gmail.com>
2022-04-14 14:55:01 -0400

    done?

--- CECS130lab10_Blake_Tigges.cpp
+++ CECS130lab10_Blake_Tigges.cpp
@@ -14,7 +14,7 @@ class game{
     public:
         void clearBoard(); // function to clear board
         void printBoard(); // function to print board
-        int checkSpot(int, int, int); // function to check spot
+        int checkSpot(int, int); // function to check spot
         void first(); // function to who goes first randomly
         void flipActive(); // change active letter
         void checkWin(); // has someone won
@@ -23,12 +23,20 @@ class game{
         int done = 0; // is game still playing
         char active = 'X'; // use x or o
     private:
+        char board[3][3]; // 3x3 array for board
+};
+class game3d : public game{
+    public:
+        void clearBoard(); // function to clear board
+        void printBoard(); // function to print board
+        int checkSpot(int, int, int); // function to check spot
+        void checkWin(); // has someone won
+    private:
         char board[3][3][3]; // 3x3x3 array for board [col][row][level]
 };
-
 int main(){
     cout << "Tic Tac Toe" << endl;
-    game tboard; // create board object
+    game3d tboard; // create board object
     tboard.clearBoard(); // clear board
     tboard.printBoard(); // print board
     tboard.first(); // who goes first
@@ -67,17 +75,8 @@ int main(){
         }
     }
 }
-void game::whoWon(){
-    if (turn == 0){ // if active is winning
-            cout << "Player wins!" << endl;
-            done = 1; // game is done
-        }
-    else if (turn == 1){ // if active is not winning
-        cout << "Computer wins!" << endl;
-        done = 1; // game is done
-    }
-}
-void game::checkWin(){
+// Override base class functions
+void game3d::checkWin(){
     // check for win
     for (int j = 0; j < 3; j++){ // check for wins on each level but only inside that level
         for (int i = 0; i < 3; i++){ // check rows
@@ -93,7 +92,7 @@ void game::checkWin(){
         if ((board[0][0][j] == 'X' || board[0][0][j] == 'O') && board[0][0][j] == board[1][1][j] && board[1][1][j] == board[2][2][j]){ // check diag 1
             whoWon(); // check who won
         }
-        else if ((board[2][0][j] == 'X' || board[2][0][j] == 'O') && board[2][0][j] == board[1][1][j] && board[1][1][j] == board[0][2][j]){ // check column 1
+        else if ((board[2][0][j] == 'X' || board[2][0][j] == 'O') && board[2][0][j] == board[1][1][j] && board[1][1][j] == board[0][2][j]){ // check daig 2
             whoWon(); // check who won
         }
     }
@@ -104,21 +103,40 @@ void game::checkWin(){
             } 
         }
     }
-    for (int i = 0; i < 3; i++){ // check rows
-            if ((board[i][0][0] == 'X' || board[i][0][0] == 'O') && board[i][0][j] == board[i][1][j] && board[i][1][j] == board[i][2][j]){
-                whoWon();
-            }
+    for (int i = 0; i < 3; i++){  
+        if ((board[i][0][0] == 'X' || board[i][0][0] == 'O') && board[i][0][0] == board[i][1][1] && board[i][1][1] == board[i][2][2]){ // top row to bottom row across levels
+            whoWon();
         }
-}
-void game::flipActive(){ // function to change active
-    if (active == 'X'){ // if active is x
-        active = 'O'; // change to o
     }
-    else if (active == 'O'){ // if active is o
-        active = 'X'; // change to x
+    for (int i = 0; i < 3; i++){ 
+        if ((board[i][2][0] == 'X' || board[i][2][0] == 'O') && board[i][2][0] == board[i][1][1] && board[i][1][1] == board[i][0][2]){ // bottom row to top row across levels
+            whoWon();
+        }
+    }
+    for (int i = 0; i < 3; i++){ 
+        if ((board[0][i][0] == 'X' || board[0][i][0] == 'O') && board[0][i][0] == board[1][i][1] && board[1][i][1] == board[2][i][2]){ // left column to right column across levels
+            whoWon();
+        }
+    }
+    for (int i = 0; i < 3; i++){ 
+        if ((board[2][i][0] == 'X' || board[2][i][0] == 'O') && board[2][i][0] == board[1][i][1] && board[1][i][1] == board[0][i][2]){ // right column to left column across levels
+            whoWon();
+        }
+    }
+    if ((board[0][0][0] == 'X' || board[0][0][0] == 'O') && board[0][0][0] == board[1][1][1] && board[1][1][1] == board[2][2][2]){ // top left to bottom right
+        whoWon();
+    }
+    if ((board[0][0][2] == 'X' || board[0][0][2] == 'O') && board[0][0][2] == board[1][1][1] && board[1][1][1] == board[2][2][0]){ // top bottom right to bottom top left
+        whoWon(); 
+    }
+    if ((board[2][0][0] == 'X' || board[2][0][0] == 'O') && board[2][0][0] == board[1][1][1] && board[1][1][1] == board[0][2][2]){ // top right to bottom left
+        whoWon();
+    }
+    if ((board[2][0][2] == 'X' || board[2][0][2] == 'O') && board[2][0][2] == board[1][1][1] && board[1][1][1] == board[0][2][0]){ // top left to bottom right
+        whoWon();
     }
 }
-int game::checkSpot(int col, int row, int lvl){
+int game3d::checkSpot(int col, int row, int lvl){
     if (board[col-1][row-1][lvl-1] != ' '){
         //cout << "Spot is taken" << endl;
         //cout << col << " " << row << endl;
@@ -128,7 +146,7 @@ int game::checkSpot(int col, int row, int lvl){
         return 1;
     }
 }
-void game::clearBoard(){
+void game3d::clearBoard(){
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             for (int k = 0; k < 3; k++){
@@ -137,7 +155,7 @@ void game::clearBoard(){
         }
     }
 }
-void game::printBoard(){
+void game3d::printBoard(){
     for (int k = 0; k < 3; k++){
         cout << endl << "  1 2 3" << endl;
         for(int i = 0; i < 3; i++){
@@ -146,6 +164,71 @@ void game::printBoard(){
                 cout << board[j][i][k] << " ";
             }
             cout << endl;
+        }
+        cout << endl;
+    }
+}
+// Base class functions
+void game::whoWon(){
+    if (turn == 0){ // if active is winning
+            cout << "Player wins!" << endl;
+            done = 1; // game is done
+        }
+    else if (turn == 1){ // if active is not winning
+        cout << "Computer wins!" << endl;
+        done = 1; // game is done
+    }
+}
+void game::checkWin(){
+    // check for win
+    for (int i = 0; i < 3; i++){ // check rows
+        if ((board[i][0] == 'X' || board[i][0] == 'O') && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+            whoWon();
+        }
+    }
+    for (int i = 0; i < 3; i++){ // check colums
+        if ((board[0][i] == 'X' || board[0][i] == 'O') && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+            whoWon();
+        }
+    }
+    if ((board[0][0] == 'X' || board[0][0] == 'O') && board[0][0] == board[1][1] && board[1][1] == board[2][2]){ // check diag 1
+        whoWon(); // check who won
+    }
+    else if ((board[2][0] == 'X' || board[2][0] == 'O') && board[2][0] == board[1][1] && board[1][1] == board[0][2]){ // check column 1
+        whoWon(); // check who won
+    }
+}
+void game::flipActive(){ // function to change active
+    if (active == 'X'){ // if active is x
+        active = 'O'; // change to o
+    }
+    else if (active == 'O'){ // if active is o
+        active = 'X'; // change to x
+    }
+}
+int game::checkSpot(int col, int row){
+    if (board[col-1][row-1] != ' '){
+        //cout << "Spot is taken" << endl;
+        //cout << col << " " << row << endl;
+        return 0;
+    } else {
+        board[col -1][row -1] = active;
+        return 1;
+    }
+}
+void game::clearBoard(){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            board[i][j] = ' ';
+        }
+    }
+}
+void game::printBoard(){
+    cout << endl << "  1 2 3" << endl;
+    for(int i = 0; i < 3; i++){
+        cout << i+1 << " ";
+        for(int j = 0; j < 3; j++){
+            cout << board[j][i] << " ";
         }
         cout << endl;
     }
