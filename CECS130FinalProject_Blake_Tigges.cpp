c7ef6291fe14b00ed4570ccae2b4b7ddd2dcd639
Blake <ihackedurtoaster@gmail.com>
2022-04-16 14:18:39 -0400

    start final project

---
+++ CECS130FinalProject_Blake_Tigges.cpp
@@ -0,0 +1,29 @@
+/*
+Final Project - Symptom Checker
+4/16/2022
+Section 1A
+Blake Tigges 
+*/
+
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+using namespace std; 
+
+int main(){
+    cout << "Welcome to Symptom Checker" << endl << endl;
+    cout << "Guide: \t* Common \t+ Sometimes/Rarely \t- No" << endl;
+    cout << "=========================================================" << endl;
+    cout << "Symptoms\t|\tCOVID-19  |   Cold   |   Flu   |" << endl;
+    cout << "=========================================================" << endl;
+    cout << "Fever\t\t|\t* \t  |    +     |   -      |" << endl;
+    cout << "Cough\t\t|\t* \t  |    +     |   -      |" << endl;
+    cout << "Shortness of Breath|\t* \t  |    +     |   -      |" << endl;
+    cout << "Runny Nose\t|\t* \t  |    +     |   -      |" << endl;
+    cout << "Headaches\t|\t* \t  |    +     |   -      |" << endl;
+    cout << "Sneezing\t|\t* \t  |    +     |   -      |" << endl;    
+    cout << "Fatigue\t\t|\t* \t  |    +     |   -      |" << endl;
+    cout << "=========================================================" << endl;
+
+
+}
